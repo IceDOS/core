2ecc07a82f7b003facafbcd096b115643b742309
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-10 14:38:08 +0300

    refactor(options): make option attrset appendable

--- lib.nix
+++ lib.nix
@@ -10,6 +10,40 @@ let
   myLib = rec {
     INPUTS_PREFIX = "icedos";
 
+    inherit (lib) mkOption types;
+
+    mkBoolOption = args: mkOption (args // { type = types.bool; });
+    mkLinesOption = args: mkOption (args // { type = types.lines; });
+    mkNumberOption = args: mkOption (args // { type = types.number; });
+    mkStrListOption = args: mkOption (args // { type = with types; listOf str; });
+    mkStrOption = args: mkOption (args // { type = types.str; });
+
+    mkSubmoduleAttrsOption =
+      args: options:
+      mkOption (
+        args
+        // {
+          type = types.attrsOf (
+            types.submodule {
+              options = options;
+            }
+          );
+        }
+      );
+
+    mkSubmoduleListOption =
+      args: options:
+      mkOption (
+        args
+        // {
+          type = types.listOf (
+            types.submodule {
+              options = options;
+            }
+          );
+        }
+      );
+
     filterByAttrs =
       let
         inherit (lib)
