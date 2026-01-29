a1109156ff55860d1085ba3674ccba8c9f0027a9
jim3692 <jim3692@gmail.com>
2026-01-29 22:28:07 +0200

    add validations

--- flake.nix
+++ flake.nix
@@ -13,8 +13,22 @@
       lib.mkIceDOS =
         { configRoot, stateDir ? ".state" }:
         let
-          inherit (builtins) readFile;
-          inherit (fromTOML (readFile "${configRoot}/config.toml")) icedos;
+          inherit (builtins) isString pathExists readFile throw;
+          inherit (nixpkgs.lib) isDerivation;
+
+          isFlake = value: (value._type or null) == "flake";
+
+          _configRoot =
+            if ((isFlake configRoot) && (pathExists "${configRoot}/flake.nix") && (pathExists "${configRoot}/config.toml"))
+            then (configRoot)
+            else (throw "The value of `configRoot` is invalid. Please set `configRoot = self;`.");
+
+          _stateDir =
+            if (isString stateDir)
+            then (stateDir)
+            else (throw "The value of `stateDir` should be a string.");
+
+          inherit (fromTOML (readFile "${_configRoot}/config.toml")) icedos;
 
           system = icedos.system.arch or "x86_64-linux";
           pkgs = nixpkgs.legacyPackages.${system};
@@ -37,7 +51,7 @@
             export PATH="${makeBinPath [ git nh nix nixfmt-rfc-style rsync ]}:$PATH"
             export ICEDOS_ROOT="${self}"
             export ICEDOS_CONFIG_ROOT="$PWD"
-            export ICEDOS_STATE_DIR="$PWD/${stateDir}"
+            export ICEDOS_STATE_DIR="$PWD/${_stateDir}"
             mkdir -p "$ICEDOS_STATE_DIR"
 
             [ -f "$ICEDOS_STATE_DIR/build.sh" ] && rm "$ICEDOS_STATE_DIR/build.sh"
