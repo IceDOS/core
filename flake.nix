5dfbe7ab915ae57766f9b84b2c2cb7f73c7f8615
IceDBorn <git.outsider841@simplelogin.fr>
2026-01-31 04:17:12 +0200

    use not deprecated nixfmt plus formatting

--- flake.nix
+++ flake.nix
@@ -11,22 +11,34 @@
     }:
     {
       lib.mkIceDOS =
-        { configRoot, stateDir ? ".state" }:
+        {
+          configRoot,
+          stateDir ? ".state",
+        }:
         let
-          inherit (builtins) isString pathExists readFile throw;
-          inherit (nixpkgs.lib) isDerivation;
+          inherit (builtins)
+            isString
+            pathExists
+            readFile
+            throw
+            ;
 
           isFlake = value: (value._type or null) == "flake";
 
           _configRoot =
-            if ((isFlake configRoot) && (pathExists "${configRoot}/flake.nix") && (pathExists "${configRoot}/config.toml"))
-            then (configRoot)
-            else (throw "The value of `configRoot` is invalid. Please set `configRoot = self;`.");
+            if
+              (
+                (isFlake configRoot)
+                && (pathExists "${configRoot}/flake.nix")
+                && (pathExists "${configRoot}/config.toml")
+              )
+            then
+              configRoot
+            else
+              (throw "The value of `configRoot` is invalid. Please set `configRoot = self;`.");
 
           _stateDir =
-            if (isString stateDir)
-            then (stateDir)
-            else (throw "The value of `stateDir` should be a string.");
+            if (isString stateDir) then (stateDir) else (throw "The value of `stateDir` should be a string.");
 
           inherit (fromTOML (readFile "${_configRoot}/config.toml")) icedos;
 
@@ -38,30 +50,40 @@
             lib
             nh
             nix
-            nixfmt-rfc-style
+            nixfmt
             rsync
             writeShellScript
             ;
 
           inherit (lib) makeBinPath;
 
-          icedosBuild = toString (writeShellScript "icedos-build" ''
-            set -e
+          icedosBuild = toString (
+            writeShellScript "icedos-build" ''
+              set -e
 
-            export PATH="${makeBinPath [ git nh nix nixfmt-rfc-style rsync ]}:$PATH"
-            export ICEDOS_ROOT="${self}"
-            export ICEDOS_CONFIG_ROOT="$PWD"
-            export ICEDOS_STATE_DIR="$PWD/${_stateDir}"
-            mkdir -p "$ICEDOS_STATE_DIR"
+              export PATH="${
+                makeBinPath [
+                  git
+                  nh
+                  nix
+                  nixfmt
+                  rsync
+                ]
+              }:$PATH"
+              export ICEDOS_ROOT="${self}"
+              export ICEDOS_CONFIG_ROOT="$PWD"
+              export ICEDOS_STATE_DIR="$PWD/${_stateDir}"
+              mkdir -p "$ICEDOS_STATE_DIR"
 
-            [ -f "$ICEDOS_STATE_DIR/build.sh" ] && rm "$ICEDOS_STATE_DIR/build.sh"
-            echo "#!/usr/bin/env bash" >>"$ICEDOS_STATE_DIR/build.sh"
-            echo "set -e" >>"$ICEDOS_STATE_DIR/build.sh"
-            echo "cd \"$PWD\"" >>"$ICEDOS_STATE_DIR/build.sh"
-            echo "nix run . -- \"\$@\"" >>"$ICEDOS_STATE_DIR/build.sh"
+              [ -f "$ICEDOS_STATE_DIR/build.sh" ] && rm "$ICEDOS_STATE_DIR/build.sh"
+              echo "#!/usr/bin/env bash" >>"$ICEDOS_STATE_DIR/build.sh"
+              echo "set -e" >>"$ICEDOS_STATE_DIR/build.sh"
+              echo "cd \"$PWD\"" >>"$ICEDOS_STATE_DIR/build.sh"
+              echo "nix run . -- \"\$@\"" >>"$ICEDOS_STATE_DIR/build.sh"
 
-            bash "${self}/build.sh" "$@"
-          '');
+              bash "${self}/build.sh" "$@"
+            ''
+          );
         in
         {
           apps.${system}.default = {
