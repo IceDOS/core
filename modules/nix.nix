15e055bfbebefbf2928f9633e9ece4927d83c3bc
IceDBorn <git.outsider841@simplelogin.fr>
2026-04-28 16:31:09 +0300

    feat(nix): add run command in the pkgs toolset

--- modules/nix.nix
+++ modules/nix.nix
@@ -60,6 +60,97 @@ in
             bash -c "$BUILD $RUN"
           '';
         }
+
+        {
+          command = "run";
+          help = "build a nixpkgs attribute and exec its main binary";
+          script = ''
+            ${colorBashHeader}
+
+            export NIXPKGS_ALLOW_UNFREE=1
+
+            if [[ ${helpFlags} ]]; then
+              echo "Available arguments:"
+              echo -e "> ${purpleString "<package>"}: nixpkgs attribute name (e.g. firefox, git, nodejs)"
+              echo -e "> ${purpleString "-s|--select"}: show binary selector even when a main program is set"
+              echo -e "> ${purpleString "--insecure"}: allow insecure packages"
+              echo -e "> ${purpleString "--"}: end of icedos flags; everything after is forwarded to the launched binary"
+              exit 0
+            fi
+
+            SELECT=0
+            PACKAGE=""
+            declare -a BIN_ARGS=()
+
+            while [[ $# -gt 0 ]]; do
+              case "$1" in
+                -s|--select)
+                  SELECT=1
+                  shift
+                  ;;
+                --insecure)
+                  export NIXPKGS_ALLOW_INSECURE=1
+                  shift
+                  ;;
+                --)
+                  shift
+                  BIN_ARGS+=("$@")
+                  break
+                  ;;
+                *)
+                  if [ -z "$PACKAGE" ]; then
+                    PACKAGE="$1"
+                  else
+                    BIN_ARGS+=("$1")
+                  fi
+                  shift
+                  ;;
+              esac
+            done
+
+            [ -z "$PACKAGE" ] && echo -e "${redString "error"}: package name is required" && exit 1
+
+            if [ "$SELECT" -ne 1 ]; then
+              EXE=$(nix eval --raw --impure --expr \
+                "(let p = (import <nixpkgs> {}); in p.lib.getExe p.$PACKAGE)" \
+                2>/dev/null) || EXE=""
+
+              if [ -n "$EXE" ] && [ -x "$EXE" ]; then
+                exec "$EXE" "''${BIN_ARGS[@]}"
+              fi
+            fi
+
+            STORE_PATH=$(nix-build '<nixpkgs>' --no-out-link -A "$PACKAGE" 2>/dev/null) || {
+              echo -e "${redString "error"}: failed to build package '$PACKAGE'"
+              exit 1
+            }
+
+            BIN_DIR="$STORE_PATH/bin"
+            if [ ! -d "$BIN_DIR" ]; then
+              echo -e "${redString "error"}: '$PACKAGE' has no /bin directory"
+              exit 1
+            fi
+
+            mapfile -t BINS < <(ls "$BIN_DIR" 2>/dev/null | sort)
+
+            if [ ''${#BINS[@]} -eq 0 ]; then
+              echo -e "${redString "error"}: no executables in $BIN_DIR"
+              exit 1
+            fi
+
+            if [ ''${#BINS[@]} -eq 1 ]; then
+              exec "$BIN_DIR/''${BINS[0]}" "''${BIN_ARGS[@]}"
+            fi
+
+            echo "Binaries in $PACKAGE:"
+            PS3="Select binary: "
+            select bin in "''${BINS[@]}"; do
+              if [ -n "$bin" ]; then
+                exec "$BIN_DIR/$bin" "''${BIN_ARGS[@]}"
+              fi
+            done
+          '';
+        }
       ];
     }
 
