09a566fd23b4622fce3168216c5530a04fdce157
IceDBorn <git.outsider841@simplelogin.fr>
2026-08-27 19:44:05 +0300

    fix(build): zsh module failing with stylix disabled

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -9,7 +9,8 @@ let
   inherit (config.icedos.system.zsh) aliases enable;
   inherit (lib) mkIf readFile replaceStrings;
 
-  stylixColors = config.lib.stylix.colors;
+  # stylix is desktop-only; skip color patching and use native p10k colors.
+  stylixColors = if config.lib ? stylix then config.lib.stylix.colors else null;
 
   p10kColorTargets = [
     "local red='#FF5C57'"
@@ -20,6 +21,7 @@ let
     "local white='#F1F1F0'"
   ];
 
+  # base16 slots: 08 red, 0A yellow, 0D blue, 0E magenta, 0C cyan, 07 white.
   p10kColorReplacements = [
     "local red='#${stylixColors.base08}'"
     "local yellow='#${stylixColors.base0A}'"
@@ -29,7 +31,11 @@ let
     "local white='#${stylixColors.base07}'"
   ];
 
-  p10kThemeText = replaceStrings p10kColorTargets p10kColorReplacements (readFile ./p10k-theme.zsh);
+  p10kThemeText =
+    if stylixColors == null then
+      readFile ./p10k-theme.zsh
+    else
+      replaceStrings p10kColorTargets p10kColorReplacements (readFile ./p10k-theme.zsh);
 in
 {
   config = mkIf enable {
